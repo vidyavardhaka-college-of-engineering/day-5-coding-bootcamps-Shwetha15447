560b412478ff9e941b42d62dac50623bb62aa9aa
Shwetha15447 <shwethasgowdashwethasgowda@gmail.com>
2021-02-19 11:53:00 +0000

    reverse a string done by bhuvan,gowrav and shwetha

--- Program_2.c
+++ Program_2.c
@@ -5,10 +5,27 @@
 
 int main()
 {
-	char str[10];
+	char str[10],rev[10];
+  int count=0,begin,end;
 	scanf("%s", str);
 
+
 	//Write your code here
+  while(str[count]!='\0')
+  count++;
+
+  end = count-1;
+
+  for(begin=0;begin<count;begin++)
+  {
+    rev[begin] = str[end];
+    end--;
+  }
+  rev[begin] = '\0';
+  printf("%s\n",rev);
+
+
+
 
 	return 0;
 }
